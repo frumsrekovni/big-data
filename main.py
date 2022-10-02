a26b9074aea8b1afd31a9de0972548b4c8302780
frumsrekovni <joshua.backstrom@gmail.com>
2022-10-02 23:50:42 +0200

    -Learning more basic ways of specifying data fetching

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pandas as pd
 # Use pandas.head(number) to look at the top of data
 # Use pandas.tail(number) to look at the bottom of data
 # Use .columns to print out the all the column titles
-# Use pandas['name_of_column'][0:10] to get a specific column and within a specific range
+# Use df['name_of_column'][0:10] to get a specific column and within a specific range
+# df.iloc[1:4] is another way to specify rows
+# You can specify exact columns like this df[['name1','name2','name3']]
+# df.loc[df['column-name'] == "desired-value-in-column"] 
+# is a way to print out all elements that share that same value in the same column.
+# For example print((df.loc[df['HP'] == 90])) would print out all rows where the HP column has the value of 90
 
 # There are built in pandas functions for csv(read_csv('')), excel(read_excel('')) and txt files.
 # txt files uses the read_csv but with a delimiter. 
@@ -10,4 +15,6 @@ import pandas as pd
 df = pd.read_csv('pokemon_data.csv')
 #df_excel = pd.read_excel('pokemon_data.xlsx')
 
-print(df['HP'][0:10])
+# print(df.head(4))
+
+print((df.loc[df['HP'] == 90]))
