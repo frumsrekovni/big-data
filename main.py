5d6ee35bb91aa3926a0c689e31c230d4d555557c
frumsrekovni <joshua.backstrom@gmail.com>
2022-10-10 23:47:07 +0200

    -It is not best practice to iterate over each rows in pandas. defeats the main strenght of even using that library. -in native Python implementing a kanji counter is relatively easy, need to find a way to parse an excel file to a single string

--- main.py
+++ main.py
@@ -25,7 +25,24 @@ import pandas as pd
 # There are built in pandas functions for csv(read_csv('')), excel(read_excel('')) and txt files.
 # txt files uses the read_csv but with a delimiter. 
 
+#print(df['人名/本名'].value_counts())
 df = pd.read_excel('data/excel/japanese-people-data.xlsx')
-#df_excel = pd.read_excel('pokemon_data.xlsx')
+df = df.reset_index()
 
-print(df[0:2])
+# totkawa = 0
+# for index, row in df.iterrows():
+#     totkawa += row.str.count('三')
+
+# print(int(totkawa))
+# df["Indexes"]= df["人名/本名"].str.find('三')
+
+
+# df = pd.read_csv('pokemon_data.csv')
+# print(df['Type 1'].value_counts())
+datastring = "人名/本名	生年月日浅田 真央	1990年9月25日荒川 静香 	1981年12月29日シェーファーアヴィ幸樹 	1998年1月28日須藤 元気 	1978年3月8日風間靖幸	1970年2月1日宇野 昌磨	1997年12月17日武田 幸三	1972年12月27日白井 健三	1996年8月24日松井 秀喜	1974年6月12日小比類巻 貴之	1977年11月7日桜井 政博	1970年8月3日岩田 聡	1959年12月6日"
+enddata = {}
+  
+for characterindata in datastring:
+    enddata[characterindata] = enddata.get(characterindata, 0) + 1
+
+print ("Frequency of each Kanji : \n"+ str(enddata))
